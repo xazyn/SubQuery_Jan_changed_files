f997fc0ef5edf1ad2d36eec16c0f3c8b280fa62a
xazyn <98230986+xazyn@users.noreply.github.com>
2022-01-23 16:47:04 +0300

    Create exercise_1_preparation.sh

---
+++ exercise_1_preparation.sh
@@ -0,0 +1,10 @@
+curl -s https://raw.githubusercontent.com/xazyn/xazyn_api/main/xazyn_logo.sh | bash && sleep 2
+echo '\n\e[42mSubQuery January - Module 4 Exercise 1\e[0m\n' && sleep 2
+cd $HOME
+git clone https://github.com/xazyn/Subquery-4-1-prepared.git 
+cd tutorials-simple-aggregation 
+yarn install 
+yarn codegen 
+yarn build 
+docker-compose pull 
+docker-compose up
