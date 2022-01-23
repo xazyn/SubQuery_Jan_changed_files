5220caf06875d2135f01cab38de85157174619e0
xazyn <98230986+xazyn@users.noreply.github.com>
2022-01-23 17:02:02 +0300

    Create exercise_4_change.sh

---
+++ exercise_4_change.sh
@@ -0,0 +1,15 @@
+curl -s https://raw.githubusercontent.com/xazyn/xazyn_api/main/xazyn_logo.sh | bash && sleep 2
+echo '\n\e[42mSubQuery January - Module 4 Exercise 4\e[0m\n' && sleep 2
+sudo rm -r $HOME/exercise_3_change.sh
+sudo rm -r $HOME/tutorials-simple-aggregation/.data 
+sudo rm -r $HOME/tutorials-simple-aggregation/schema.graphql 
+sudo rm -r $HOME/tutorials-simple-aggregation/project.yaml 
+sudo rm -r $HOME/tutorials-simple-aggregation/src/mappings/mappingHandlers.ts 
+wget -q -O $HOME/tutorials-simple-aggregation"/schema.graphql" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/ex4_schema.graphql" 
+wget -q -O $HOME/tutorials-simple-aggregation"/project.yaml" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/ex4_project.yaml" 
+wget -q -O $HOME/tutorials-simple-aggregation/src/mappings"/mappingHandlers.ts" "https://raw.githubusercontent.com/cryptongithub/Subquery-4-prepared_file_change/main/ex4_mappingHandlers.ts" 
+cd $HOME/Subquery-4-1-prepared
+yarn codegen 
+yarn build 
+docker-compose pull 
+docker-compose up
